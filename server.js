2d6d541d92ce5555fae243b1ecd8b42589a1444f
Glitch (hello-sqlite) <none>
2019-06-11 05:26:46 +0000

    🎷🌸 Checkpoint ./server.js:4973547/374

--- server.js
+++ server.js
@@ -8,11 +8,17 @@ const appPort = process.env.PORT
 
 // Set Webhook
 bot.telegram.setWebhook(`https://${appName}.glitch.me/webhook`)
-console.log(`Listening incoming webhook on: https://${appName}.glitch.me/webhook`)
 
+bot.telegram.getMe().then((botInfo) => {
+  bot.options.username = botInfo.username
+})
 
 // Start builtin Webhook
 bot.startWebhook('/webhook', null, appPort)
 
 // Listener
-bot.on('text', (ctx) => ctx.reply('Hello Human'))
+//bot.on('text', (ctx) => ctx.reply('Hello Human'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.command('help', (ctx) => ctx.replyWithMarkdown(helpResponse));
+
+console.log(`Listening incoming webhook on: https://${appName}.glitch.me/webhook`)
