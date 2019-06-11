53787e459f261d7a7ca90af984f4c0f09665291b
Glitch (hello-sqlite) <none>
2019-06-11 05:04:46 +0000

    🌯🙏 Checkpoint ./server.js:4973547/2596 ./package.json:4973547/22

--- server.js
+++ server.js
@@ -1,61 +1,24 @@
-// server.js
-// where your node app starts
+const Botgram = require('botgram');
+const figlet = require('figlet');
 
-// init project
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+if (!TELEGRAM_BOT_TOKEN) {
+  console.error('Seems like you forgot to pass Telegram Bot Token. I can not proceed...');
+  process.exit(1);
+}
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+const bot = new Botgram(TELEGRAM_BOT_TOKEN);
 
-// init sqlite db
-var fs = require('fs');
-var dbFile = './.data/sqlite.db';
-var exists = fs.existsSync(dbFile);
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(dbFile);
-
-// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-db.serialize(function(){
-  if (!exists) {
-    db.run('CREATE TABLE Dreams (dream TEXT)');
-    console.log('New table Dreams created!');
-    
-    // insert default dreams
-    db.serialize(function() {
-      db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
-    });
-  }
-  else {
-    console.log('Database "Dreams" ready to go!');
-    db.each('SELECT * from Dreams', function(err, row) {
-      if ( row ) {
-        console.log('record:', row);
-      }
-    });
-  }
-});
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// endpoint to get all the dreams in the database
-// currently this is the only endpoint, ie. adding dreams won't update the database
-// read the sqlite3 module docs and try to add your own! https://www.npmjs.com/package/sqlite3
-app.get('/getDreams', function(request, response) {
-  db.all('SELECT * from Dreams', function(err, rows) {
-    response.send(JSON.stringify(rows));
+function onMessage(msg, reply) {
+  figlet(msg.text, (err, data) => {
+    if (err) {
+      reply.text('An error occured. Probably text format is not correct.').then();
+      return;
+    }
+    const markdownResult = `${'```\n'}${data}${'\n```'}`;
+    reply.markdown(markdownResult).then();
   });
-});
+}
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+bot.text(onMessage);
