f931ede5ac872e85d1318f533531fd003a627de5
Glitch (hello-sqlite) <none>
2019-06-12 09:29:15 +0000

    🏪💳 Checkpoint ./commands.js:4973547/993

--- commands.js
+++ commands.js
@@ -1,5 +1,5 @@
 const util = require('./util');
-const https = require('request');
+const request = require('request');
 
 const Extra = require('telegraf/extra')
 
@@ -68,7 +68,11 @@ commands.push({
     let api_key = process.env.GOOGLE_SEARCH_API_KEY;
     let cx = process.env.GOOGLE_SEARCH_CX;
     let url = `https://www.googleapis.com/customsearch/v1?key=${api_key}&cx=${cx}&q=${text.split(' ').join('+')}`;
-    ctx.reply(url);
+    request.get(url, function (error, response, body) {
+      if (!response || response.statusCode != 200) {
+        console.log(response)
+      }
+    });
   }
 });
 
