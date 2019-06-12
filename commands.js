daedd3ce86e2e48863b81a2b3c4403964251c027
Glitch (hello-sqlite) <none>
2019-06-12 07:08:15 +0000

    💠👣 Checkpoint ./commands.js:4973547/4270

--- commands.js
+++ commands.js
@@ -8,45 +8,50 @@ function setCommands(bot) {
   bot.command(test.name, (ctx) => test.do(ctx));
 }
 
-const check = {};
-check.name = 'check';
-check.do = function (ctx) {
-  try {
-    let result;
-    let procent = ('' + (Math.random() * 100)).split(".")[0] + '%';
-    let text = util.cutTextCommand(ctx.message.text, check.name);
-    
-    if (!text) {
-      result = 'Пустой запрос, бака не спамь o(≧口≦)o o(≧口≦)o o(≧口≦)o';
-      return;
-    }
+const commands = {
+  check: {
+    do: function (ctx) {
+      try {
+        let result;
+        let procent = ('' + (Math.random() * 100)).split(".")[0] + '%';
+        let text = util.cutTextCommand(ctx.message.text, check.name);
 
-    if (ctx.message.entities.length > 1 && (ctx.message.entities[1].type === 'text_mention' || ctx.message.entities[1].type === 'mention')) {
-      if (ctx.message.entities[1].type === 'text_mention') {
-        text = text.replace(ctx.message.entities[1].user.first_name, ('<a href="tg://user?id=' + ctx.message.entities[1].user.id + '">' + ctx.message.entities[1].user.first_name + '</a>'));
+        if (!text) {
+          result = 'Пустой запрос, бака не спамь o(≧口≦)o o(≧口≦)o o(≧口≦)o';
+          return;
+        }
+
+        if (ctx.message.entities.length > 1 && (ctx.message.entities[1].type === 'text_mention' || ctx.message.entities[1].type === 'mention')) {
+          if (ctx.message.entities[1].type === 'text_mention') {
+            text = text.replace(ctx.message.entities[1].user.first_name, ('<a href="tg://user?id=' + ctx.message.entities[1].user.id + '">' + ctx.message.entities[1].user.first_name + '</a>'));
+          }
+          result = text + ' на ' + procent;
+        }
+        else {
+          result = `<a href="tg://user?id=${ctx.message.from.id }"> ${ctx.message.from.first_name}</a> ${text} на <b>${procent}</b>`;
+        }
+        ctx.parse_mode = 'HTML';
+        ctx.reply(result, Extra.HTML());
+        }
+      catch (e) {
+        console.log('Ooops', e)
       }
-      result = text + ' на ' + procent;
     }
-    else {
-      result = `<a href="tg://user?id=${ctx.message.from.id }"> ${ctx.message.from.first_name}</a> ${text} на <b>${procent}</b>`;
+  },
+  test: {
+    do: function (ctx)  {
+      //ctx.reply(JSON.stringify(ctx.message, null, 4));
+      ctx.reply(arguments.callee.toString());
     }
-    ctx.parse_mode = 'HTML';
-    ctx.reply(result, Extra.HTML());
+  },
+  search: {
+    do: function (ctx)  {
+      let text = util.cutTextCommand(ctx.message.text, search  .name);
     }
-  catch (e) {
-    console.log('Ooops', e)
   }
-}
-
-const test = {};
-test.name = 'test';
-test.do = function (ctx)  {
-  ctx.reply(JSON.stringify(ctx.message, null, 4));
-}
+};
 
 const search = {};
 search.name = 'search';
-search.do = function (ctx)  {
-  let text = util.cutTextCommand(ctx.message.text, search  .name);
-}
+search.
 module.exports.setCommands = setCommands;
