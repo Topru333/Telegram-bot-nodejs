ba0dd4c9dcba4586b7eff616f7fd9d846e79a81f
Glitch (hello-sqlite) <none>
2019-06-12 00:16:23 +0000

    🏞🚅 Checkpoint ./server.js:4973547/536

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const Telegraf = require('telegraf')
 // Bot TOKEN
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN, {username: process.env.TELEGRAM_BOT_USER_NAME})
 
-const appName = process.env.PROJECT_NAME
+const appName = 'telegram-weeb-bot'
 const appPort = process.env.PORT
 
 // Set Webhook
-//bot.telegram.setWebhook(`https://${appName}.glitch.me/webhook`)
+bot.telegram.setWebhook(`https://${appName}.glitch.me/webhook`)
 
 //bot.use((ctx, next) => {
 //  ctx.reply('test');
@@ -30,7 +30,7 @@ bot.startWebhook('/webhook', null, appPort)
 
 // Listener
 //bot.on('text', (ctx) => ctx.reply('Hello Human'))
-bot.hears('сука', (ctx) => ctx.reply('блять'));
+bot.hears('пинг', (ctx) => ctx.reply('понг'));
 commands.setCommands(bot);
 
 console.log(`Listening incoming webhook on: https://${appName}.glitch.me/webhook`)
